ab4fc173d02b2bbd2eaeb4351129717278e4a0e2
omoshthegeek <omoshthegeek@gmail.com>
2019-11-10 19:15:48 +0300

    added order constructors

--- js/script.js
+++ js/script.js
@@ -1,71 +1,62 @@
-var itemCount = 0;
-var priceTotal = 0;
-var pricelist = {
-  price1:[500,700,950],
-  price2:[600,800,1050]
-}
 
 
+$(document).ready(function () {
+  var itemCount = 0;
+  var priceTotal = 0;
+  var pricelist = {
+    price1: [500, 700, 950],
+    price2: [600, 800, 1050]
+  }
 
+  $('.order').click(function () {
+    //incrementing quantity
+    itemCount++;
 
-var crust = $('#crust option:selected').val();
-      var topping = $('#topping option:selected').val();
-      var size = $('#size option:selected').val();
-      var qty = itemCount;
-      var delivery=$('#delivery option:selected').val();
-
-
-$(document).ready( function(){
-  $('#pap').click( function(){
-    prompt("Enter Delivery address")
-  });
-    $('.order').click(function (){
-        itemCount ++;
-
-      
-
-     
-
-      // alert(crust)
-      // alert(topping)
-      // alert(size)
-      // alert(qty)
-      // alert(delivery)
-
-
-        var order =  {
-          c
-
-          topping = topping;
-          size = size;
-         qty = qty
-          delivery= delivery;
-        }  
-        
-        alert (order());
-
-        
-        
-        
-
-
-        
-
-       
-      
-        $('#itemCount').text(itemCount).css('display', 'block');
-        
-      
+    var crust1 = $('#crust option:selected').val();
+    var topping1 = $('#topping option:selected').val();
+    var size1 = $('#size option:selected').val();
+    var quantity1 = itemCount;
+    var delivery1 = $('#delivery option:selected').val();
+    
   
-    })
-    $('.cart').click(function(){
-        itemCount --;
-        $('#itemCount').text(itemCount).css('display', 'block');
-        if (itemCount < 1 ) {
-          $('#itemCount').css('display', 'none');
-          itemCount=0;
-        }
-      }
-    )
+  //order constructor
+    function Order(crust, topping, size, quantity, delivery) {
+      this.crust = crust;
+      this.topping = topping;
+      this.size = size;
+      this.quantity = quantity;
+    }
+  
+    var custOrder = new Order(crust1, topping1, size1, quantity1, delivery1);
+    
+    //checking price depending on order
+    
+  
+  
+  
+  
+    
+  
+  
+    
+
+
+
+
+
+    $('#itemCount').text(itemCount).css('display', 'block');
+
+
+
+  })
+  $('.cart').click(function () {
+    itemCount--;
+    $('#itemCount').text(itemCount).css('display', 'block');
+    if (itemCount < 1) {
+      $('#itemCount').css('display', 'none');
+      itemCount = 0;
+    }
+  }
+  )
 
 })
