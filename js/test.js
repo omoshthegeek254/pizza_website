db7ec17c8246b0ab37f01b8fe13add4a208e26a7
omoshthegeek <omoshthegeek@gmail.com>
2019-11-11 12:07:12 +0300

    added the test elements

--- js/test.js
+++ js/test.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
+
+    $('.order').click(function (){
+        itemCount ++;
+
+      var crust = $('#crust option:selected').val();
+      var topping = $('#topping option:selected').val();
+      var size = $('#size option:selected').val();
+    //   var qty = itemCount;
+    //   var delivery=$('#delivery option:selected').val();
+
+      alert(crust+"-"+topping+"-"+size);
+      
+    
+
+
+     
+
+       
+      
+        $('#itemCount').text(itemCount).css('display', 'block');
+        
+      
+  
+    })
         $(".deliver").click(
             function(){
                 var crust = $("#crust option:selected").val();
